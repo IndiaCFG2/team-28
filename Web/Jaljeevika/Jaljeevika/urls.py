82b435830c7c2e23099fee222b16d1d5e1afa622
Ameeruddin <ameeruddin9848@gmail.com>
2020-08-08 20:57:58 +0530

    made changes in html pages

--- Web/Jaljeevika/Jaljeevika/urls.py
+++ Web/Jaljeevika/Jaljeevika/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+
 urlpatterns = [
 	path('',include('dashboard.urls')),
     path('admin/', admin.site.urls),
--- Web/Jaljeevika/dashboard/views.py
+++ Web/Jaljeevika/dashboard/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
-
+from .models import fpoproduct
 # Create your views here.
-def render_marketview(request):
-	return render(request,'base.html')
+
 	
-def render_fpodashboard(request):
+def marketview(request):
+	products = fpoproduct.objects.all().order_by('-created')
+	#,{'products':products}
+	return render(request,'marketview.html')
+
+def home(request):
+	return render(request,'home.html')
+
+def fpodashboard(request):
 	return render(request,'fpodashboard.html')
 
-def render_forum(request):
+
+def knowledge(request):
+	return render(request,'knowledge.html')
+
+def forum(request):
 	return render(request,'forum.html')
 
-def render_marketplace(request):
-	return render(request,'marketplace.html')
+def gallery(request):
+	return render(request,'gallery.html')
+
+def about(request):
+	return render(request,'about.html')
+
+
