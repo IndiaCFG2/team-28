ee7ed5a9e07599bff0b346f298d1cd6373fd3402
Your Name <nikhilthadeshwar2@gmail.com>
2020-08-08 20:07:33 +0530

    changes

--- Web/Jaljeevika/dashboard/urls.py
+++ Web/Jaljeevika/dashboard/urls.py
@@ -4,9 +4,8 @@ from django.urls import path,include,re_path
 from . import views
 
 urlpatterns = [
-	url(r'^marketview/$',views.render_marketview,name='marketview'),
-	url(r'^fpodashboard/$',views.render_fpodashboard,name='fpodashboard'),
-	url(r'^forum/$',views.render_forum,name='forum'),
+	url(r'^fpodashboard/$',views.fpodashboard,name='fpodashboard'),
+	url(r'^forum/$',views.forum,name='forum'),
 	url(r'^about/$',views.about,name='about'),
 	url(r'^gallery/$',views.gallery,name='gallery'),
 	url(r'^knowledge/$',views.knowledge,name='knowledge'),
