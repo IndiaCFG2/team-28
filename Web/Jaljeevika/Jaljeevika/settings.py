c2c5c2357da178a037ae872713941d5858349c14
Ameeruddin <ameeruddin9848@gmail.com>
2020-08-08 19:55:16 +0530

    removed errors in database models

--- Web/Jaljeevika/Jaljeevika/settings.py
+++ Web/Jaljeevika/Jaljeevika/settings.py
@@ -114,7 +114,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'Jaljeevika/media')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
