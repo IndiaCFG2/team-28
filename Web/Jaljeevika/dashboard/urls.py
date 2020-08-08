4c68e3cd8efaf9544651c540db3094ca042cfe64
Nikhil Thadeshwar <NIKHILTHADESHWAR2@GMAIL.COM>
2020-08-08 18:39:46 +0530

    Update urls.py

--- Web/Jaljeevika/dashboard/urls.py
+++ Web/Jaljeevika/dashboard/urls.py
@@ -4,8 +4,12 @@ from django.urls import path,include,re_path
 from . import views
 
 urlpatterns = [
-	url(r'^marketview/$',views.render_marketview,name='marketview'),
-	url(r'^fpodashboard/$',views.render_fpodashboard,name='fpodashboard'),
-	url(r'^forum/$',views.render_forum,name='forum'),
-	url(r'^marketplace/$',views.render_marketplace,name='marketplace'),
+	url(r'^marketview/$',views.marketview,name='marketview'),
+	url(r'^fpodashboard/$',views.fpodashboard,name='fpodashboard'),
+	url(r'^forum/$',views.forum,name='forum'),
+	url(r'^marketplace/$',views.marketplace,name='marketplace'),
+	url(r'^about/$',views.about,name='about'),
+	url(r'^gallery/$',views.gallery,name='gallery'),
+	url(r'^knowledge/$',views.knowledge,name='knowledge'),
+	url(r'^contact/$',views.contact,name='contact'),
 ]
