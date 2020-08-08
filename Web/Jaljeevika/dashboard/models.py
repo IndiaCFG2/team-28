dd918c2fcf32ea886e35706073d7ce782b363fa4
Ameeruddin <ameeruddin9848@gmail.com>
2020-08-08 14:40:20 +0530

    added database models for fpo and product

--- Web/Jaljeevika/dashboard/models.py
+++ Web/Jaljeevika/dashboard/models.py
@@ -12,3 +12,37 @@ class ForumComment(models.Model):
 
     def __str__(self):
         return self.comment
+
+
+
+
+class fpoproduct(models.Model):
+	pid=models.AutoField(primary_key=True)
+	productname=models.CharField(max_length=200, default='')
+	fponame=models.CharField(max_length=200, default='')
+	description=models.CharField(max_length=200, default='')
+	timeline=models.DateField(default=date.today)
+	quantity=models.CharField(max_length=200, default='')
+	price=models.IntegerField(default=0)
+	fid= models.ForeignKey(FPO, on_delete=models.CASCADE)
+
+
+
+class FPO(models.Model):
+	fid=models.CharField(max_length=200, default='')
+	statename=models.CharField(max_length=200, default='')
+	district=models.CharField(max_length=200, default='')
+	programme=models.CharField(max_length=200, default='')
+	resource=models.CharField(max_length=200, default='')
+	name=models.CharField(max_length=200, default='')
+	legalformfpo=models.CharField(max_length=200, default='')
+	date= models.DateField( default=date.today)
+	address=models.CharField(max_length=300, default='')
+	crops=models.ListCharField(
+        base_field=CharField(max_length=10),
+        size=6,
+        max_length=(6 * 11)  
+    )
+	contact=models.CharField(max_length=300, default='')
+
+
